e577cc8bd925bb9711b9ff529d3c883cd4383e97
pmorhun <petermorhun@gmail.com>
2016-02-15 15:27:32 +0200

    ticket:1 create model Peraon

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    """Person Model"""
+
+    first_name = models.CharField(max_length=256, blank=False)
+    last_name = models.CharField(max_length=256, blank=False)
+    birthday = models.DateField(blank=False, null=True)
+    bio = models.TextField()
+    email = models.EmailField()
+    jabber = models.CharField(max_length=256)
+    skype = models.CharField(max_length=256)
+    contacts = models.TextField()
+
+    def __unicode__(self):
+        return u"%s %s" % (self.first_name, self.last_name)
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+from apps.hello.models import Person
+
+
+class PersonView(ListView):
+    model = Person
+    template_name = 'hello/persons.html'
+    context_object_name = 'persons'
+
+    def get_queryset(self):
+        persons = Person.objects.all()
+        return persons
+
