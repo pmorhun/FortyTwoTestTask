5db15f0fde04db013861f52b62ca5a25d23fbbb0
pmorhun <petermorhun@gmail.com>
2016-02-17 16:51:03 +0200

    ticket:1 test fail for view with model

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
+from django.template.defaultfilters import date
+from apps.hello.models import Person
 
 
 class TestPersonView(TestCase):
@@ -15,6 +17,25 @@ class TestPersonView(TestCase):
         self.assertIn('Petro', response.content)
         self.assertIn('p.etros', response.content)
         self.assertEqual(len(response.context['persons']), 1)
+
+
+class PersonViewTest(TestCase):
+    fixtures = ['init_data.json']
+
+    def test_hello(self):
+        """Testing response view"""
+        response = self.client.get(reverse('hello'))
+        self.assertEqual(response.status_code, 200)
+
+        data = Person.objects.get()
+        self.assertContains(response, data.first_name)
+        self.assertContains(response, data.last_name)
+        self.assertContains(response, date(data.birthday))
+        self.assertContains(response, data.bio)
+        self.assertContains(response, data.email)
+        self.assertContains(response, data.jabber)
+        self.assertContains(response, data.skype)
+        self.assertContains(response, data.contacts)
 
 
 class BadResponse(TestCase):
