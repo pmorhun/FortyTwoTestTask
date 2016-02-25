ff4e35aed69843407af5064f942b7095d85167af
pmorhun <petermorhun@gmail.com>
2016-02-25 12:18:28 +0200

    ticket:1 edited tests

--- apps/contact/tests.py
+++ apps/contact/tests.py
@@ -5,21 +5,21 @@ from apps.contact.models import Person
 
 
 class PersonViewTest(TestCase):
-
     def test_contact(self):
         """Testing response view"""
         response = self.client.get(reverse('contact'))
         self.assertEqual(response.status_code, 200)
-
-        td = Person.objects.get()
-        self.assertContains(response, td.first_name)
-        self.assertContains(response, td.last_name)
-        self.assertContains(response, date(td.birthday))
-        self.assertContains(response, td.bio)
-        self.assertContains(response, td.email)
-        self.assertContains(response, td.jabber)
-        self.assertContains(response, td.skype)
-        self.assertContains(response, td.other_contacts)
+        data = Person.objects.get(id=1)
+        self.assertContains(response, data.first_name)
+        self.assertContains(response, data.last_name)
+        self.assertContains(response, date(data.birthday))
+        self.assertContains(response, data.email)
+        self.assertContains(response, data.skype)
+        self.assertContains(response, data.jabber)
+        self.assertContains(response, data.bio)
+        self.assertContains(response, data.other_contacts)
+        count = Person.objects.all().count()
+        self.assertEqual(count, 1)
 
 
 class BadResponse(TestCase):
@@ -28,3 +28,10 @@ class BadResponse(TestCase):
         bad_response = self.client.get('/blah')
         self.assertEqual(bad_response.status_code, 404)
         self.assertTemplateUsed(bad_response, '404.html')
+
+
+class PersonModelTests(TestCase):
+    def test_unicode(self):
+        """Test person model"""
+        person = Person(first_name='Test', last_name='Testerenko')
+        self.assertEqual(unicode(person), u'Test Testerenko')
--- apps/contact/views.py
+++ apps/contact/views.py
@@ -5,8 +5,8 @@ from apps.contact.models import Person
 class PersonView(ListView):
     model = Person
     template_name = 'contact/contacts.html'
-    context_object_name = 'persons'
+    context_object_name = 'person'
 
     def get_queryset(self):
-        persons = Person.objects.all()
+        persons = Person.objects.get(id=1)
         return persons
